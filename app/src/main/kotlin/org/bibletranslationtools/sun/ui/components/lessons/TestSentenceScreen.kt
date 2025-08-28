c4b180cd66119b43708640f94be7c8f4e6826076
Maxim <murmax82@gmail.com>
2025-08-28 16:55:13 +0400

    images shrunk

--- app/src/main/kotlin/org/bibletranslationtools/sun/ui/components/lessons/TestSentenceScreen.kt
+++ app/src/main/kotlin/org/bibletranslationtools/sun/ui/components/lessons/TestSentenceScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
@@ -56,7 +55,7 @@ fun TestSentenceScreen(component: TestSentenceComponent) {
             ) {
                 QuestionSection(
                     imageUri = model.imageUri,
-                    modifier = Modifier.height(400.dp)
+                    modifier = Modifier.weight(1f)
                 )
 
                 AnswerArea(
